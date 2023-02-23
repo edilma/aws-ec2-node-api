00e95978424973dab060ea4dc444ca119342d64d
Edilma Riano <edilmaria@hotmail.com>
2023-02-23 15:38:10 -0500

    CREATED an api for aws

---
+++ index.js
@@ -0,0 +1,11 @@
+import  express from "express";
+import cors from "cors"
+import "dotenv/config"
+
+const app = express()
+    app.use(cors())
+    app.use(express.json())
+
+    app.listen('5002',()=>console.log("Listening on port 5000"))
+
+    app.get("/",(req,res)=>res.send("API working 🤓"))
